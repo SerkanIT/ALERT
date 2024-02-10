0e887ac3ee17785e226ece6573f881c37bf3e131
SerkanIT <157528566+SerkanIT@users.noreply.github.com>
2024-02-10 16:33:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// // '', "", ``, Math +, , -, *,/, 
+// // typeof = type of text or string
+
+let about = alert("Let's take information about you :)")
+let n = prompt("Write your name")
+let l = prompt("Write your surname")
+let f = prompt("In which course do you study?")
+let random = (Math.floor, Math.random() *10)
+let a = prompt("Year:")
+let b = prompt("Your Birthday:")
+let age = a - b
+alert("Your Age: " + age )
+confirm("All the information is correct?");
+
+
+console.log("Course", f);
+let abouts = alert("Okey, information is correctly taken. Virus is downloading...")
+let viruss = confirm("Wait!")
+let success = alert("Virus downloaded successfully.")
+let goodluck = alert("Good Luck to your computer. Because it die :)")
+let joke = alert("It's a joke!")
+let jokes = alert("HAHA xD")
+// alert("Your ID is: ") + n + l + f + a;
+alert(` \n 1) Your Name is :  ${n} \n 2) Your Surname is :  ${l} \n 3) Your Age is: ${age} \n 4)  Your course is :  ${f} \n 5) Your ID is : ${random} `)
+// let aming = "Serkan"
+// let yosh = 12
+// let country = "Turkey"
+
+// console.log(naming + ": " + age);
+// console.log(`${naming} yoshim ${yosh} yashiman ${country}`);
+// let mathMethods = Math.ceil(2.3)
+// console.log(mathMethodss);
+
+// let test = prompt("What is your name \n 1) Serkan \n 2) Sunnatilla \n 3) Diyora")
+ 
+
+
+// Конкаменация
+
+// Math methodts ceil(), floor(), round(), random()
+// Генерируем случайный идентификационный номер
+
